2c31fdd54703ec12b2d9315438a0e90c823b05fd
Katarina Popovic <kaca.popovic000@gmail.com>
2024-01-19 00:41:39 +0100

    Add Model and implemented ResNet Architecture

--- data.py
+++ data.py
@@ -5,6 +5,7 @@ from skimage.io import imread
 from skimage.color import gray2rgb
 import numpy as np
 import torchvision as tv
+import os
 
 train_mean = [0.59685254, 0.59685254, 0.59685254]
 train_std = [0.16043035, 0.16043035, 0.16043035]
@@ -14,13 +15,42 @@ class ChallengeDataset(Dataset):
     def __init__(self, data, mode):
         self.data = data
         self.mode = mode
-        self._transform = tv.tranfsorms.Compose([tv.transforms.ToTensor()])
+        self._transform = tv.transforms.Compose([tv.transforms.ToPILImage(),  # Use only if the input image is not a PIL image
+                                                 tv.transforms.ToTensor(),
+                                                 tv.transforms.Normalize(mean=[train_mean], std=[train_std])])
+
+    @property
+    def transform(self):
+        return self._transform
+
+    @transform.setter
+    def transform(self, transformation):
+        self._transform = tv.transforms.Compose(transformation)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
+
+        # Load labels
         if torch.is_tensor(index):
             index = index.tolist()
+        sample = self.data.iloc[index]
+        relative_path = sample["filename"]
+        crack_label = sample["crack"]
+        inactive_label = sample["inactive"]
 
-        pass
+        # Load image
+        script_directory = os.path.dirname(os.path.abspath(__file__))
+        # Construct the absolute path to the image
+        absolute_path_to_image = os.path.join(script_directory, relative_path)
+        image = imread(absolute_path_to_image)
+
+        # Transform image
+        image = gray2rgb(image)
+        image = self.transform(image)
+
+        # Stack labels into tensor
+        labels = torch.tensor([crack_label, inactive_label])
+
+        return image, labels
---
+++ model.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        # Skip connection
+        self.skip = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.skip = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        identity = self.skip(x)
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        out += identity
+        out = F.relu(out)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3,64,7,2)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.max_pool = nn.MaxPool2d(3,2)
+        self.res1 = ResBlock(64,64,1)
+        self.res2 = ResBlock(64,128,2)
+        self.res3 = ResBlock(128, 256, 2)
+        self.res4 = ResBlock(256, 512, 2)
+        self.global_pool = nn.AdaptiveAvgPool2d(1,1)
+        self.fc = nn.Linear(512,2)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.max_pool(out)
+        out = self.res1(out)
+        out = self.res2(out)
+        out = self.res3(out)
+        out = self.res4(out)
+        out = self.global_pool(out)
+        out = torch.flatten(out)
+        out = self.fc(out)
+        out = F.sigmoid(out)
+        return out
+
